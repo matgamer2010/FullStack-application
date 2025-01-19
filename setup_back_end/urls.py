d3c33fe4c9cdf21f9eb9cd37c4b93e13590f5076
Mateus Montanher <157072446+matgamer2010@users.noreply.github.com>
2025-01-18 22:26:13 -0300

    Update urls.py
    Implementing the routers

--- setup_back_end/urls.py
+++ setup_back_end/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from setup_back_end.settings import MEDIA_ROOT, MEDIA_URL
+from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
+from Api_Clothes.views import Crud
+
+
+router = DefaultRouter()
+router.register(r'Crud', Crud, basename='Crud_clothes')
 
 urlpatterns = [
+    path("", include(router.urls)),
     path('admin/', admin.site.urls),   
-    path('', include("Api_Clothes.urls")),
-] + static(MEDIA_URL, document_root=MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
