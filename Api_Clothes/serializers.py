5690aac36bc6028a06cc7bb876f0a2a01deda3c9
Mateus Montanher <157072446+matgamer2010@users.noreply.github.com>
2025-01-18 22:23:07 -0300

    Update serializers.py
    Enhancing the API displaying, making availiable each endpoint of each object.

--- Api_Clothes/serializers.py
+++ Api_Clothes/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from Api_Clothes.models import DataBaseClothes
 
-class SerializersClothes(serializers.ModelSerializer):
-    class meta():
-      model = DataBaseClothes
-      fields = '__all__'
+class SerializersClothes(serializers.HyperlinkedModelSerializer):
+  url = serializers.HyperlinkedIdentityField(view_name='Crud_clothes-detail')
+  class Meta:
+    model = DataBaseClothes
+    fields = '__all__'
