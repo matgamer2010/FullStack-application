3679ceaea2cb225ff47231414a87212c15d814fc
Mateus Montanher <157072446+matgamer2010@users.noreply.github.com>
2025-01-18 22:24:51 -0300

    Update admin.py
    Adapting the Admin

--- Api_Clothes/admin.py
+++ Api_Clothes/admin.py
@@ -1,20 +1,10 @@
 from django.contrib import admin
 from Api_Clothes.models import DataBaseClothes
-import json 
-
-def get_json_admin_files():
-    with open("Api_Clothes/sizes.json") as file_admin:
-      data = json.load(file_admin)
 
 class AdminClothes(admin.ModelAdmin):
-  def get_list_display(self, request):
-    base_fields = ["name","price", "public","date"]
-    dynamic_fields = []
-    
-    for sizes in get_json_fields:
-      field_name = f"is_size{sizes}"
-      dynamic_fields.append(field_name)
-      #add a "str()" primitive tipe above.
-
-    return base_fields + dynamic_fields
+  list_display = ("id","name", "price", "public", "date", "size_gg", "size_g", "size_m", "size_p", "size_pp")
+  list_display_links = ("id","name",)
+  search_fields = ("name",)
+  list_editable = ("public",)
+ 
 admin.site.register(DataBaseClothes, AdminClothes)
