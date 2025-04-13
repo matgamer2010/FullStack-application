e7d5d27d020373bfda73e184f3ecdc5760e4a456
matgamer2010 <matgamer297@gmail.com>
2025-04-12 23:45:21 -0300

    Setting up the Allowed URLs

--- setup_back_end/settings.py
+++ setup_back_end/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["meu-backend-django.onrender.com", "localhost", "127.0.0.1",]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "fullstack-application-5s0h.onrender.com"]
 
 
 
