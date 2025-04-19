52cb499667a01be64f718b11f0d69cdfe1b80163
matgamer2010 <matgamer297@gmail.com>
2025-04-19 20:11:58 -0300

    Cache integration was successful

--- setup_backend_node/routes/product.js
+++ setup_backend_node/routes/product.js
@@ -1,30 +1,65 @@
 import {prisma} from "../src/prisma";
 import redis from "../src/redis";
 import express from "express";
+import axios from "axios"
 
 const router = express.Router();
 
-router.get('/products', async (request, res) =>{
-    try{
-        const products = await prisma.products.findMany();
-        res.json(products);
-    } catch{
-        res.status(500).json({error: "Ther was an error with the request"})        
-    }
-});
-
+// Redis deals with Products' requests
 router.get('/products', async (request, response) =>{
-    const cachedProducts = await redis.get('products');
+    const cache = await redis.get("products");
 
-    if(cachedProducts){
-        console.log('Dados da requisição foram capturados com o Redis')
-        return response.json(JSON.parse(cachedProducts));
+    if(cache){
+        return response.json(JSON.parse(cache));
     }
 
-    const products = await prisma.product.findMany();
-    await redis.set('products', JSON.stringify(products), 'EX', 120);
-    console.log("Dados do Prisma (prismaMM)");
-    response.json(products);
+    const { data: requestAPI } = await axios.get("http://127.0.0.1:8000/products/");
+    await redis.set("products", JSON.stringify(requestAPI), "EX", 90);
+    return response.json(requestAPI);
 });
+
+// Prisma deals with others ways of requests
+
+router.get('/users/', async (request, response) =>{
+    const cachePrisma = await prisma.UserForCache.findMany();
+
+    const now = new Date();
+    const firstUser = cachePrisma[0];
+
+    if(firstUser){
+        const timeVerificate = (now- new Date(firstUser.cachedAt) / 1000);
+
+        if(timeVerificate > 90){
+            await prisma.UserForCache.deleteMany();
+            // URL for Users
+            const {data} = await axios.get("http://127.0.0.1:8000/users/");
+
+            for(let user of data){
+                await prisma.UserForCache.create({
+                    data:{
+                        name: user.name,
+                        email: user.email,
+                        cachedAt: now,
+                    },
+                });
+                return response.json(data);
+            }
+        } else{
+            return response.json(cachePrisma);
+        }
+    } else{
+        const {data} = await axios.get("http://127.0.0.1:8000/users/");
+        for(let user of data){
+            await prisma.UserForCache.create({
+                data:{
+                    name: user.name,
+                    email: user.email,
+                    cachedAt: now,
+                }
+            })
+        }
+    }
+});
+
 
 export default router;
