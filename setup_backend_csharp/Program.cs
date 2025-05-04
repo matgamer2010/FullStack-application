9092bad323791321bd89e59581904bf64d02de70
matgamer2010 <matgamer297@gmail.com>
2025-05-03 22:02:54 -0300

    CORS correction

--- setup_backend_csharp/Program.cs
+++ setup_backend_csharp/Program.cs
@@ -24,7 +24,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000") // ou sua URL do frontend
+            policy.WithOrigins("https://mm-vendedores.vercel.app/") // ou sua URL do frontend
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
